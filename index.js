9892fc311b8fdeae2b37788b7969ca480cdff49d
Basil Homadi <deviltearbasil@gmail.com>
2020-09-18 13:26:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,3 +40,42 @@ function createEmployeeRecords(srArr){
      myObj2.timeOutEvents.push(timeOut);
      return myObj2;
  }
+ 
+ function hoursWorkedOnDate(empinfo, date){
+    let startDate = empinfo.timeInEvents.find(e=>e.date === date);
+    let endDate = empinfo.timeOutEvents.find(e=>e.date === date);
+    let startHour= startDate.hour;
+    let endHour= endDate.hour;
+    let workHours=  (endHour-startHour)/100;
+    return workHours;
+ }
+
+ function wagesEarnedOnDate(empinfo, date){
+     let workHours= hoursWorkedOnDate(empinfo, date);
+     let hourPayment = empinfo.payPerHour;
+     let payRate = workHours * hourPayment;
+     return payRate;
+ }
+
+ function allWagesFor(empinfo){
+     let avDate=[];
+    for(let i = 0; i < empinfo.timeInEvents.length; i++){
+       avDate.push(wagesEarnedOnDate(empinfo, empinfo.timeInEvents[i].date))
+    }
+    let fullPayment = avDate.reduce(function (e, storage) {
+        return e + storage}, 0);
+    return fullPayment;
+ }
+
+ function calculatePayroll(recArr){
+     let allPayment=0;
+     for (let i=0; i<recArr.length; i++){
+         allPayment= allPayment+allWagesFor(recArr[i])
+     }
+     return allPayment;
+ }
+
+ function findEmployeeByFirstName(recArr,firstName){
+     let record= recArr.find(e=> e.firstName===firstName)
+     return record;
+ }
