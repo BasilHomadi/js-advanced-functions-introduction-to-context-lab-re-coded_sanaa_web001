3b1966feb676f5b88006e8a27318b27713d6c047
Basil Homadi <deviltearbasil@gmail.com>
2020-09-18 13:25:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function createEmployeeRecords(srArr){
      let recor = srArr.map(e=>createEmployeeRecord(e))
  return recor;
  }
+ 
+ function createTimeInEvent(empinfo,time){
+     let myTime= time.split(' ');
+     let timeIn ={
+         type: "TimeIn",
+         hour: parseInt(myTime[1]),
+         date: myTime[0]
+     }
+      let myObj= Object.assign({}, empinfo);
+      myObj.timeInEvents.push(timeIn);
+      return myObj;
+ }
