9bbe20459abcdfd134ace5adfe4e665e82a6b3e5
Basil Homadi <deviltearbasil@gmail.com>
2020-09-18 13:24:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function createEmployeeRecord(srArr){
+
+    let empInfo={
+        firstName: srArr[0],
+        familyName: srArr[1],
+        title: srArr[2],
+        payPerHour: srArr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return empInfo;
+ }
+
+function createEmployeeRecords(srArr){
+     let recor = srArr.map(e=>createEmployeeRecord(e))
+ return recor;
+ }
